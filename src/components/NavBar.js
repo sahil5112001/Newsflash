bbc50924a1579adb62e36395b0cd59a98e9156ea
sahil5112001 <112097642+sahil5112001@users.noreply.github.com>
2023-07-29 12:17:25 +0530

    add prev and next button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ export class NavBar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/">NewsFlash</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,10 +23,7 @@ export class NavBar extends Component {
                                 <li className="nav-item">
                                     <a className="nav-link" href="/about">About</a>
                                 </li>
-                                
-
                             </ul>
-
                         </div>
                     </div>
                 </nav>
